e19183eefdb9c6a5f45818e3f4c6e94c1e1b3c1b
Flutter Zone <letsflatter@gmail.com>
2021-04-03 18:02:35 +0000

    changed styles of custom buttons to jsx

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from "../../firebase/firebase.utils";
 import {connect} from 'react-redux';
@@ -9,7 +8,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
 import {selectCurrentUser} from '../../redux/user/user.selectors';
 
-import {HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink} from './header.styles';
+import {HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
 
 const Header = ({ currentUser, hidden  }) => (
     <HeaderContainer>
@@ -20,7 +19,7 @@ const Header = ({ currentUser, hidden  }) => (
             <OptionLink to='/shop'>SHOP</OptionLink>
             <OptionLink to='/shop'>CONTACT</OptionLink>
             {
-                currentUser ? <OptionDiv onClick={() => auth.signOut() }>SIGN OUT</OptionDiv> : <OptionLink to='/signin'>SIGN IN</OptionLink>
+                currentUser ? <OptionLink as='div' onClick={() => auth.signOut() }>SIGN OUT</OptionLink> : <OptionLink to='/signin'>SIGN IN</OptionLink>
             }
             <CartIcon/>
         </OptionsContainer>
